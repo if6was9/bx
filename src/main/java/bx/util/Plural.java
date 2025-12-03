f223cabb1bf5482a9ce11afd41e014454118a54c
Rob Schoening <robschoening@gmail.com>
2025-12-02 20:20:16 -0800

    add some plural rules

--- src/main/java/bx/util/Plural.java
+++ src/main/java/bx/util/Plural.java
@@ -7,12 +7,32 @@ public class Plural {
   static Map<String, String> pluralMap =
       Map.of(
           "man", "men", "woman", "women", "person", "people", "crypto", "crypto", "index",
-          "indices");
+          "indices", "goose", "geese", "roof", "roofs", "photo", "photos", "piano", "pianos",
+          "axis", "axes");
 
   public static String toPlural(String singular) {
     String pluralName = pluralMap.get(singular.toLowerCase());
     if (pluralName != null) {
       return pluralName;
+    }
+
+    if (singular.endsWith("o")) {
+      return singular + "es";
+    }
+    if (singular.endsWith("fe")) {
+      return singular.substring(0, singular.length() - 2) + "ves";
+    }
+    if (singular.endsWith("f")) {
+      return singular.substring(0, singular.length() - 1) + "ves";
+    }
+    if (singular.endsWith("ch")) {
+      return singular + "es";
+    }
+    if (singular.endsWith("y")) {
+      return singular.substring(0, singular.length() - 1) + "ies";
+    }
+    if (singular.toLowerCase().endsWith("s")) {
+      return String.format("%ses", singular);
     }
     return String.format("%ss", singular);
   }
